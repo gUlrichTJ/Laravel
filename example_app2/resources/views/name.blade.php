a36906cc42d8208535f861c607ea5226236408cd
gUlrichTJ <gracianomathey@gmail.com>
2023-04-19 16:40:01 +0000

    Commit

---
+++ example_app2/resources/views/name.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    {{ $number }}
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse dolores aspernatur dolore porro veniam maiores eaque quibusdam culpa, nesciunt facere deleniti vel, molestias a incidunt aperiam sunt est aut doloribus.
+</body>
+</html>
--- example_app2/routes/web.php
+++ example_app2/routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::get('/blog',function(){
+        return view('name',
+        [
+            'number'=>2
+        ]
+    );
+    }
+);
